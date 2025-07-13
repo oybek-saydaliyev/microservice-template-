f3aa527ada6b80f5156f0c3fb2f9943750eab1ac
oybek <saydaliyev.dev@gmail.com>
2025-07-13 12:39:29 +0500

    initial commit for -spring microservice architecture

---
+++ auth-servcie/src/main/java/uz/project/authservice/controller/AuthController.java
@@ -0,0 +1,16 @@
+package uz.project.authservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    @GetMapping("/test")
+    public String test() {
+        return "auth-service/test";
+    }
+
+}
---
+++ auth-servcie/src/test/java/uz/project/authservice/AuthServcieApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.project.authservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServcieApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
